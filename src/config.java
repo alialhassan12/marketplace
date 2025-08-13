1c83d7f6a67324a2e68380cda141a9e92aa7a96c
SF-47 <samihfarhat2005@gmail.com>
2025-08-13 10:16:56 +0300

    test

--- src/config.java
+++ src/config.java
@@ -1,11 +1,12 @@
 import java.sql.*;
 public class config {
-    private static String url="jdbc:sqlserver://DESKTOP-JO4LG69\\SQLEXPRESS02;databaseName=CarManagementDB;encrypt=false";
-    private static String username="sa";
-    private static String password="root";
+private static String url = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres";
+    private static String username="postgres.jjrtlhqvivxhiuorlauo";
+    private static String password="d@t@b@$eiul2025";
     public static Connection getConnection(){
         Connection con=null;
         try{
+            Class.forName("org.postgresql.Driver");
             con=DriverManager.getConnection(url, username, password);
             System.out.println("Connection Established");
         }catch(Exception e){
